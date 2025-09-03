0c4a70a55ef13649ddf7f337acdf81a7d68e0e53
ariekas <140879155+ariekas@users.noreply.github.com>
2025-09-03 11:43:47 +0700

    Add files via upload

---
+++ MiniTask1.js
@@ -0,0 +1,52 @@
+// var objec, mendapatkan hasil koda, tapi harus akses
+
+const obj = {
+  str: [
+    "a",
+    "b",
+    "c",
+    [
+      "A",
+      [
+        "A",
+        "B",
+        {
+          man: [
+            {
+              the: {
+                name: {
+                  of: {
+                    us: "Koda",
+                  },
+                },
+              },
+            },
+          ],
+        },
+      ],
+    ],
+  ],
+};
+
+const hello = {
+  world: "Hello word",
+};
+
+const my = [
+  {
+    favourite: [
+      0,
+      1,
+      2,
+      {
+        fruit: {
+          is: "Aplle",
+        },
+      },
+    ],
+  },
+];
+
+console.log(obj.str[3][1][2].man[0].the.name.of.us);
+console.log(hello.world);
+console.log(my[0].favourite[3].fruit.is);
