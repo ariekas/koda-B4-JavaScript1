6ff24fc10b9c75004128488ae4f8c8e11b71a43e
ariekas <140879155+ariekas@users.noreply.github.com>
2025-09-03 15:57:55 +0700

    Add files via upload

---
+++ MiniTask2.js
@@ -0,0 +1,12 @@
+// buatkan saya segitiga siku siku
+
+for(let Baris = 0; Baris < 7; Baris++)
+{
+    let Simbol = ""
+    for(let Tambah = 0; Tambah < Baris; Tambah++){
+        Simbol += "*"
+    }
+    
+    console.log( Simbol)
+}
+// proses pemgabungan strin
